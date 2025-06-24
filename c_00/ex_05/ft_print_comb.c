45302301926663601d21613c9274ce3de59b4f89
walid-chihab <walid-chihab@users.noreply.github.com>
2025-06-24 19:13:59 +0100

    fixation

--- c_00/ex_05/ft_print_comb.c
+++ c_00/ex_05/ft_print_comb.c
@@ -10,22 +10,26 @@
 /*					                                */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
 
-void ft_putchar(char c) {
-    write(1, &c, 1);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void ft_print_comb(void) {
-    int i = 0;
+void	ft_print_comb(void)
+{
+    int	i=0 ;
     while (i <= 7) {
-        int j = i + 1;
-        while (j <= 8) {
-            int k = j + 1;
-            while (k <= 9) {
-                ft_putchar(i + '0');
-                ft_putchar(j + '0');
-                ft_putchar(k + '0');
+        int	j=i+1 ;
+        while ( j<=8 )
+	{
+            int	k=j+1 ;
+            while ( k<=9 )
+	    {
+                ft_putchar( i+'0' ) ;
+                ft_putchar( j+'0' ) ;
+                ft_putchar( k+'0' ) ;
 
-                if (!(i == 7 && j == 8 && k == 9)) {
+                if ( !(i == 7 && j == 8 && k == 9)) {
                     write(1, ", ", 2);
                 }
                 k++;
@@ -37,6 +41,6 @@ void ft_print_comb(void) {
 }
 int main ()
 {
-	ft_print_comb3();//preferant la 3 emme
+	ft_print_comb3();
 	return 0;
 }
