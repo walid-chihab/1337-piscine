16471f95c571617032478f6f26657dcda8342689
walid-walid <its.n.chihab@gmail.com>
2025-06-25 22:26:57 +0100

    Update ft_putnbr.c

--- c_00/ex_07/ft_putnbr.c
+++ c_00/ex_07/ft_putnbr.c
@@ -1,20 +1,18 @@
 #include <unistd.h>
 
-void ft_putchar (char c )
+void	ft_putchar(char c)
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
-
-void ft_putnbr(int nbr)
+void	ft_putnbr(int nbr)
 {
-	if (nbr == -2147483648)
+	if(nbr == -2147483648)
 	{
-		ft_putchar ('-');//on affich les deux premeir nombre du chiffre separement 
-		ft_putchar ('2');
+		ft_putchar('-');//on affich les deux premeir nombre du chiffre separement 
+		ft_putchar('2');
 		ft_putnbr(147483648);//puis on l'integre le nbr dans le 3 emme if
 		return ; //car on peut pas traiter les autre condition autre fois 
 	}
-
 	if (nbr < 0)
 	{
 		ft_putchar('-');//affich signe moin puis changer signe du nbr et le integrer dans le condition suivant ``/
@@ -22,11 +20,11 @@ void ft_putnbr(int nbr)
 	}
 
 	if (nbr >= 10)
-		ft_putnbr (nbr /10);
-
+		ft_putnbr(nbr /10);
 	ft_putchar (nbr % 10 + '0');// sous forme caracter
 
 }
 int main (){
 	ft_putnbr(-2147483648);
+	return 0;
 }
